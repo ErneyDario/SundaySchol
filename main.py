337121326959ee22421d4e7b0d8fbc8cbe2d8faf
Erney Dario Garcia Prado <erneydariogarcia@gmail.com>
2024-03-08 19:48:38 -0500

    Creando los posts y funciones para insertar registros

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from registers.toRegister import *
 #Instaciamos nuestra app
 app = FastAPI() 
 
+#------------------------------------------
+# creamos un nuevo registro
+#------------------------------------------
 @app.post("/addstaff")
-def addPerson():
-    cursor.execute("INSERT INTO staff(Identificacion, Nombres, Apellidos, Direccion, Telefono, FechaNacimiento) VALUES ('1065570485', 'Carolina Andrea', 'Amador Peña', 'Carrera 12 11 23', '3182345678','23/04/2001')")
-    conexion.commit()
-    return("Registro exitoso")
+def addstaff():
+    return addPerson()
+    
+
 
 
 
--- registers/toRegister.py
+++ registers/toRegister.py
@@ -1,6 +1,16 @@
 from conexionDB.conexion import *
 
 def addPerson():
-    cursor.execute("INSERT INTO staff(Identificacion, Nombres, Apellidos, Direccion, Telefono, FechaNacimiento) VALUES ('1065570484', 'Andrea Johana', 'Amador Peña', 'Carrera 12 11 23', '3182345678','23/04/2001')")
+    cursor.execute("INSERT INTO staff(Identificacion, Nombres, Apellidos, Direccion, Telefono, FechaNacimiento) VALUES ('1065570490', ' Matilde lina', 'Vides Jimenez', 'Carrera 12 11 23', '3182345678','23/04/2001')")
     conexion.commit()
-    return("Registro exitoso")
+    return("El Registro fue exitoso")
+
+def addUsers():
+    cursor.execute ("INSERT INTO user(idPerson, user_Name, password) values('1065570488', 'Mati','12345678')")
+    conexion.commit()
+    return("Usuario Registrado")
+
+def addRol():
+    cursor.execute ("INSERT INTO rol(idPerson, user_Name, password) values('1065570488', 'Mati','12345678')")
+    conexion.commit()
+    return("Usuario Registrado")
